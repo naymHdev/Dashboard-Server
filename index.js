9e15fc8913d3cd5e6142121936f929b6b922abe9
MacBook Pro <macbookpro@MacBooks-MBP.Dlink>
2024-02-28 00:38:31 +0600

    create a server and install || mongodb express nodemon cors dotenv || and create amongodb server

---
+++ index.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+/// Middle Ware
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const uri = process.env.SERVER_URI;
+// console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const eiaDataCollection = client.db("EIA").collection("eiaData");
+
+    app.get("/v1/allData", async (req, res) => {
+      const result = await eiaDataCollection.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB! 🌴"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello EIA Server! 💰");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port} 💣`);
+});
